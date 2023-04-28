9e86f64abaaad83e7d5e12987d5a64c1f11b0c72
Anthonius Munthi <me@itstoni.com>
2023-04-28 23:44:54 +0800

    fixing build

--- apps/home-assistant/ci/latest.sh
+++ apps/home-assistant/ci/latest.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-version=$(curl -sX GET "https://api.github.com/repos/home-assistant/core/releases/latest" | jq --raw-output '.tag_name' 2>/dev/null)
+version=$(curl -sX GET "https://api.github.com/repos/linuxserver/docker-homeassistant/releases/latest" | jq --raw-output '.tag_name' 2>/dev/null)
 version="${version#*v}"
 version="${version#*release-}"
+version="${version//-*/}"
 printf "%s" "${version}"
