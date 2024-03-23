5aeb0b4dd56ba3a63a93e2da5e6ef2b9b0ed2eda
Anthonius Munthi <me@itstoni.com>
2024-03-23 08:41:04 +0800

    wip

--- apps/frankenphp-api/entrypoint.sh
+++ apps/frankenphp-api/entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [ "$1" = 'frankenphp' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
-	if [ -z "$(ls -A 'vendor/' 2>/dev/null)" ]; then
+	if [ -z "$(ls -A 'vendor/' 2>/dev/null)" ] && [ -z "$(ls -A . 'composer.json' 2>/dev/null)" ]; then
 		composer install --prefer-dist --no-progress --no-interaction
 	fi
 
