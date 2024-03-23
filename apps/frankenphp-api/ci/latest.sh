104854cb5a54bb533324985dcf3d277956d113c2
Anthonius Munthi <me@itstoni.com>
2024-03-23 09:20:29 +0800

    wip

--- apps/frankenphp-api/ci/latest.sh
+++ apps/frankenphp-api/ci/latest.sh
@@ -2,4 +2,5 @@
 version=$(curl -sX GET "https://api.github.com/repos/dunglas/frankenphp/releases/latest" | jq --raw-output '.tag_name' 2>/dev/null)
 version="${version#*v}"
 version="${version#*release-}"
-printf "%s" "${version}"
+#printf "%s" "${version}"
+printf "%s" "1.1.1-php8.3.4"
