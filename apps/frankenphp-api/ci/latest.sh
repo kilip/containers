f8b7382baea7a621e02d61a73b5f69d150555e23
Anthonius Munthi <me@itstoni.com>
2024-04-03 12:25:06 +0800

    fix frankenphp version (#92)

--- apps/frankenphp-api/ci/latest.sh
+++ apps/frankenphp-api/ci/latest.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-version=$(curl -sX GET "https://api.github.com/repos/dunglas/frankenphp/releases/latest" | jq --raw-output '.tag_name' 2>/dev/null)
-version="${version#*v}"
-version="${version#*release-}"
-#printf "%s" "${version}"
-printf "%s" "1.1.1-php8.3"
+frankenphp=$(curl -sX GET "https://api.github.com/repos/dunglas/frankenphp/releases/latest" | jq --raw-output '.tag_name' 2>/dev/null)
+frankenphp="${frankenphp#*v}"
+frankenphp="${frankenphp#*release-}"
+
+printf "%s" "${frankenphp}-php8.3"
