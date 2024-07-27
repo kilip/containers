88dba2d41b78affcdaf80abd180c7e924389148d
Anthonius Munthi <me@itstoni.com>
2024-07-28 04:30:26 +0800

    added bitwarden-cli container

--- apps/bitwarden-cli/ci/latest.sh
+++ apps/bitwarden-cli/ci/latest.sh
@@ -2,4 +2,5 @@
 version=$(curl -H "Accept: application/vnd.github+json" https://api.github.com/repos/bitwarden/clients/releases | jq  -r 'sort_by(.published_at) | reverse | .[].name | select( index("CLI") )' | sed 's:.*CLI v::' | head -n 1)
 version="${version#*v}"
 version="${version#*cli-}"
-printf "%s" "${version}"
+#printf "%s" "${version}"
+printf "%s" "2024.6.0"
